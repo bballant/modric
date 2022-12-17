d22c2369516a357496a7c1c93046f2ef64fbf77c
Brian Ballantine <bballantine@earnestresearch.com>
2022-12-16 19:36:31 -0500

    formatting

--- src/modric.c
+++ src/modric.c
@@ -1,34 +1,29 @@
-#include <stdio.h>
 #include "miniprintf.h"
 #include <stdarg.h>
+#include <stdio.h>
 
 char *show_binary(int width, int n) {
   static size_t count = (sizeof n) * 8;
   count = width < count ? width : count;
-	static char res[((sizeof n) * 8) + 1];
+  static char res[((sizeof n) * 8) + 1];
   for (int i = 0; i < count; i++) {
-    res[i] = '0' | ((n>>(count-1-i))&1);
+    res[i] = '0' | ((n >> (count - 1 - i)) & 1);
   }
   res[count] = 0;
-	return res;
+  return res;
 }
 
-void
-foo_putc(char ch) {
-  putchar(ch);
+void foo_putc(char ch) { putchar(ch); }
+
+int my_printf(const char *format, ...) {
+  va_list args;
+  int rc;
+
+  va_start(args, format);
+  rc = mini_vprintf_cooked(foo_putc, format, args);
+  va_end(args);
+  return rc;
 }
-
-int
-my_printf(const char *format,...) {
-	va_list args;
-	int rc;
-
-	va_start(args,format);
-	rc = mini_vprintf_cooked(foo_putc,format,args);
-	va_end(args);
-	return rc;
-}
-
 
 int main() {
   // printf() displays the string inside quotation
