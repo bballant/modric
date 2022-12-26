04caf17dc4f08cd43752c21c7454a4ecb0fbf266
Brian Ballantine <bballantine@earnestresearch.com>
2022-12-25 20:43:42 -0500

    removed unused files

--- src/alvarez.c
+++ src/alvarez.c
@@ -3,7 +3,6 @@
 
 #include "miniprintf.h"
 #include "modriclib.h"
-#include "jsmn.h"
 #include "cJSON.h"
 #include "cJSON_print.h"
 
@@ -91,33 +90,6 @@ void print_some_stuff(void) {
   }
 }
 
-
-
-void print_token(jsmntok_t *tok) {
-  char *typ;
-  switch (tok->type) {
-  case JSMN_UNDEFINED:
-    typ = "JSMN_UNDEFINED";
-    break;
-  case JSMN_OBJECT:
-    typ = "JSMN_OBJECT";
-    break;
-  case JSMN_ARRAY:
-    typ = "JSMN_ARRAY";
-    break;
-  case JSMN_STRING:
-    typ = "JSMN_STRING";
-    break;
-  case JSMN_PRIMITIVE:
-    typ = "JSMN_PRIMATIVE";
-    break;
-  default:
-    typ = "UNKNOWN";
-  }
-
-  printf("Token (%s), [%d, %d], %d\n", typ, tok->start, tok->end, tok->size);
-}
-
 void json_demo(void) {
   printf("This is the JSON demo.\n");
   char *res = m_read_text_file("colors.json");
@@ -135,14 +107,5 @@ void json_demo(void) {
 }
 
 int alvarez_main(int argc, char *argv[]) {
-  jsmn_parser p;
-  jsmntok_t t[128];
-  jsmn_init(&p);
-  char *res = m_read_text_file("colors.json");
-  int num_toks = jsmn_parse(&p, res, strlen(res), t, 128);
-  for (int i = 0; i < num_toks; i++ ) {
-    print_token(&t[i]);
-  }
-//  printf("HOly %s\n", res);
   return 0;
 }
